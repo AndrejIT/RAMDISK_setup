b6b91c92801bc9188d135ac1de347279fdab986e
AndrejIT <AndrejIT@rambler.ru>
2018-09-22 08:24:09 +0300

    Completely redo whole project to work with systemd

--- install.sh
+++ install.sh
@@ -1,27 +1,87 @@
 #! /bin/sh
 
 SERVICE="minetestserver"
+RAMDISKSERVICE="minetestramdisk"
+BACKUPSERVICE="minetestbackup"
+SAVESERVICE="minetestsave"
 WORLD="justtest"
 USERNAME="minetestuser"
+PERMISSIONS="$USERNAME:$USERNAME"
 MINETESTFOLDER="/home/$USERNAME/.minetest"
 WORLDFOLDER="/home/$USERNAME/.minetest/worlds/$WORLD"
 
+echo "Clean before Minetestserver RAMDISK install..."
 
+# Try to stop and clean old style init.d service or new systemd service
+service $SERVICE stop
+update-rc.d -f $SERVICE remove
+rm /etc/init.d/$SERVICE
+systemctl disable $SERVICE.service
+rm /etc/systemd/system/$SERVICE.service
+service $RAMDISKSERVICE stop
+systemctl disable $RAMDISKSERVICE.service
+rm /etc/systemd/system/$RAMDISKSERVICE.service
+service $BACKUPSERVICE stop
+systemctl disable $BACKUPSERVICE.service
+rm /etc/systemd/system/$BACKUPSERVICE.service
+systemctl disable $BACKUPSERVICE.timer
+rm /etc/systemd/system/$BACKUPSERVICE.timer
+service $SAVESERVICE stop
+systemctl disable $SAVESERVICE.service
+rm /etc/systemd/system/$SAVESERVICE.service
+systemctl disable $SAVESERVICE.timer
+rm /etc/systemd/system/$SAVESERVICE.timer
 
-cp clear_players.py $MINETESTFOLDER/
+echo "Minetestserver install..."
 
-chown $USERNAME:$USERNAME $MINETESTFOLDER/clear_players.py
+# create user for service if not exists
+if [ ! id -u $USERNAME > /dev/null 2>&1 ]; then
+    useradd -m $USERNAME
+    usermod -L $USERNAME
+    usermod -G $USERNAME user
+fi
 
+# Executable for services
+cp --no-preserve=mode,ownership minetestserver.sh /usr/local/bin/
+chmod +x /usr/local/bin/minetestserver.sh
 
+# Register game service
+cp --no-preserve=mode,ownership $SERVICE.service /etc/systemd/system/   # /usr/lib/systemd/system/ # More correct, but only works on nev servers
+systemctl enable $SERVICE.service
+
+# Adapt world directory to be used with ramdisk
+if [ ! -d $WORLDFOLDER.safe ]; then
+    mv $WORLDFOLDER $WORLDFOLDER.safe
+    mkdir $WORLDFOLDER
+    chown $PERMISSIONS $WORLDFOLDER/
+fi
+
+echo "Minetestserver RAMDISK install..."
+
+# Register RAMDISK service
+cp --no-preserve=mode,ownership $RAMDISKSERVICE.service /etc/systemd/system/
+systemctl enable $RAMDISKSERVICE.service
+
+echo "Minetestserver BACKUP install..."
+
+# Register BACKUP service and timer
+cp --no-preserve=mode,ownership $BACKUPSERVICE.service /etc/systemd/system/
+systemctl enable $BACKUPSERVICE.service
+cp --no-preserve=mode,ownership $BACKUPSERVICE.timer /etc/systemd/system/
+systemctl enable $BACKUPSERVICE.timer
+
+# Register SAVE service and timer
+cp --no-preserve=mode,ownership $SAVESERVICE.service /etc/systemd/system/
+systemctl enable $SAVESERVICE.service
+cp --no-preserve=mode,ownership $SAVESERVICE.timer /etc/systemd/system/
+systemctl enable $SAVESERVICE.timer
+
+# TODO Register ARCHIVE service and timer
+# TODO Register MAINTENANCE service and timer
+
+# Executable for maintenance
+cp --no-preserve=mode,ownership clear_players.py $MINETESTFOLDER/
 chmod +x $MINETESTFOLDER/clear_players.py
 
 
-adduser $USERNAME
-usermod -L $USERNAME
-
-usermod -G $USERNAME user
-
-cp $SERVICE /etc/init.d/
-chmod +x /etc/init.d/$SERVICE
-#update-rc.d $SERVICE defaults
-systemctl enable $SERVICE
+echo "End of Minetestserver install"
---
+++ minetestserver.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+# This script is not init.d anymore! It is bunch of functions to call from systems units!
+# Author: Andrey
+# New version 2018.08
+
+#Settings
+SERVICE="minetestserver"
+WORLD="justtest"
+USERNAME="minetestuser"
+PERMISSIONS="$USERNAME:$USERNAME"
+MINETESTFOLDER="/home/$USERNAME/.minetest"
+WORLDFOLDER="/home/$USERNAME/.minetest/worlds/$WORLD"
+INVOCATION="/usr/local/bin/minetestserver"
+OPTIONS="--worldname $WORLD --quiet"
+RAMDISKSIZE="2G"
+
+# Exit if the package is not installed
+[ -x "$INVOCATION" ] || exit 0
+
+# MINETEST SERVER
+mt_start() {
+    $INVOCATION $OPTIONS
+}
+
+# WHOLE WORLD DIRECTORY RAMDISK
+mt_ramdisk_start() {
+    if [ ! -d $WORLDFOLDER.safe ]; then
+        exit 3
+    fi
+    mount -t ramfs -o size=$RAMDISKSIZE ramfs $WORLDFOLDER
+    chown $PERMISSIONS $WORLDFOLDER
+    rsync -q -t -W -r -og --chown=$PERMISSIONS $WORLDFOLDER.safe/ $WORLDFOLDER/
+}
+mt_ramdisk_stop() {
+    rsync -q -t -W -r -og --chown=$PERMISSIONS $WORLDFOLDER/ $WORLDFOLDER.safe/
+    umount $WORLDFOLDER
+}
+
+# MINETEST WEEKLY Backup
+mt_backup() {
+    rsync -q -t -W -r -og --chown=$PERMISSIONS $WORLDFOLDER.safe/ $WORLDFOLDER.backup/
+}
+
+# MINETEST DAYLY Save
+mt_save() {
+    rsync -q -t -W -r -og --chown=$PERMISSIONS $WORLDFOLDER/ $WORLDFOLDER.safe/
+}
+
+
+#Start-Stop here
+case "$1" in
+start)
+mt_start
+;;
+ramdisk_start)
+mt_ramdisk_start
+;;
+ramdisk_stop)
+mt_ramdisk_stop
+;;
+backup)
+mt_backup
+;;
+save)
+mt_save
+;;
+
+*)
+echo "Usage: From systemd {start|ramdisk_start|mt_ramdisk_stop}"
+exit 1
+;;
+esac
+
+exit 0
